85ac0d7a8a88aa1ad703b4f2fc2b566f72a0b36c
Rohan K Nandula <19r11a0581@gcet.edu.in>
2021-06-03 15:42:57 +0530

    fixed issue for expenses chart

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session, Response
-from flask_login import logout_user
+from flask import Flask, render_template, request, redirect, url_for, Response
+#from flask_login import login_manager,logout_user
 import requests
 import boto3
 import simplejson as json
 import jsonify
-import pandas as pd
 import random
 import time
 from datetime import datetime
@@ -12,88 +11,23 @@ from datetime import datetime
 #Wallet balance API: https://ss979hyehb.execute-api.ap-south-1.amazonaws.com/WalletBalance
 app = Flask(__name__)
 random.seed()  # Initialize the random number generator
-#api to add money to wallet: https://l1gdzvb6k6.execute-api.ap-south-1.amazonaws.com/addwallamount
 
-@app.route('/')
-@app.route('/home')
-def home():
-    title = "Personal Expense Tracker"
-    
-    return render_template("index2.html", title = title)
-
-
-@app.route('/chart2')
-def chart2(chartID = 'chart_ID', chart_type = 'line', chart_height = 350):
-    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
-    title = {"text" : 'Monthly Expenses'}
-    user='nbalu@gmail.com'
-    expense_date_from = ''
-    expense_date_to = ''
-    params = "user="+user+"&expense_date_from="+expense_date_from+"&expense_date_to="+expense_date_to
-    print(params)
-    series_new=fetchExpenses(params)
-    #print(type(series_new))
-    exp_dates = []
-    for k, v in series_new.items():
-        if k == 'Items':
-            for item in series_new['Items']:
-                exp_dates.append(item['expense_date'])
-    #print("("+'\"categories\"' +": "+ str(exp_dates)+")")    
-    home_expenses = []
-    for k, v in series_new.items():
-        if k == 'Items':
-            for item in series_new['Items']:
-                home_expenses.append(item['home_expenses'])
-    seriesopen = "["
-    prefixstr = "{" + '\"name\"' + ": "
-    datastr = ", " + '\"data\"' + ": "
-    suffixstr = "},"
-    suffixstr2 = "}"
-    seriesend = "]"
-    homeexp = seriesopen + prefixstr + '\"Home Expenses\"' +  datastr + str(home_expenses) + suffixstr
-    medical_expenses = []
-    for k, v in series_new.items():
-        if k == 'Items':
-            for item in series_new['Items']:
-                home_expenses.append(item['medical_expenses'])
-    medicalexp = prefixstr + '\"Medical Expenses\"' +  datastr + str(medical_expenses) + suffixstr
-    vehicle_expenses = []
-    for k, v in series_new.items():
-        if k == 'Items':
-            for item in series_new['Items']:
-                home_expenses.append(item['vehicle_expenses'])
-    vehicleexp = prefixstr + '\"Vehicle Expenses\"' +  datastr + str(vehicle_expenses) + suffixstr2 + seriesend
-    series = (homeexp + medicalexp + vehicleexp)
-    print(series)
-    #xAxis = {"categories": ['05/25/2021', '05/26/2021', '05/27/2021']}
-    xAxis = "("+'\"categories\"' +": "+ str(exp_dates)+")"
-    print(xAxis)
-    yAxis = {"title": {"text": 'Expense'}}    
-    return render_template('index1.html', chartID=chartID, chart=chart, title=title, series=series,  xAxis=xAxis, yAxis=yAxis)
-
-@app.route('/chart')
-def chart():
-    def generate_random_data():
-        while True:
-            json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
-            yield f"data:{json_data}\n\n"
-            time.sleep(1)
-
-    return Response(generate_random_data(), mimetype='text/event-stream')
-
+# Function to add a specific expense for a user - AWS API Add Expense for a user
 def setExpenses(params): 
     print(params)
     url = "https://ket0h58q15.execute-api.ap-south-1.amazonaws.com/ExpenseAPI?"+params
     status = requests.request("GET",url)
     print(status.json())
     return status.json()
-    
+
+# Function to fetch expenses of a user - AWS API Expense Summary
 def fetchExpenses(params):
     url="https://5996kr662d.execute-api.ap-south-1.amazonaws.com/ExpenseQA?"+params
     status = requests.request("GET",url)
     print(status.json())
     return status.json()
 
+# Function to fetch wallet balance - AWS API Wallet Balance
 def walletBalance(params):
     url="https://ss979hyehb.execute-api.ap-south-1.amazonaws.com/WalletBalance?"+params
     status = requests.request("GET",url)
@@ -101,19 +35,36 @@ def walletBalance(params):
     return status.json()
 
 
-# Function to check user credentials - AWS API User
+# Function to check user credentials - AWS API User Details
 def check(user):
     url="https://nirmgp3j2c.execute-api.ap-south-1.amazonaws.com/fetchuser?user="+user  
     status = requests.request("GET",url)
     print(status.json())
     return status.json()
 
-# Function to add amount to wallet - AWS API Add Money
+# Function to add amount to wallet - AWS API Add Money to Wallet
 def updatewallet(params):
     url="https://l1gdzvb6k6.execute-api.ap-south-1.amazonaws.com/addwallamount?"+params 
     status = requests.request("GET",url)
     print(status.json())
     return status.json()
+
+@app.route('/')
+@app.route('/home')
+def home():
+    title = "Personal Expense Tracker"
+    
+    return render_template("dchart.html", title = title)
+
+@app.route('/chart-data')
+def dchart():
+    def generate_random_data():
+        while True:
+            json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
 
 @app.route('/wallet')
 def wallet():
@@ -150,27 +101,48 @@ def addmoneypage() :
         return render_template('addmoney.html', pred="Wallet has been updated successfully and your "+data)
     
 
-@app.route('/about')
-def aboutpage():
-
-    title = "About this site - PET"
-    paragraph = ["Will this chart work with dynamic values set?"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-@app.route('/nmlgraph', methods=['GET','POST'])
-def nmlgraph(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
-    user='nbalu@gmail.com'
-    expense_date_from = ''
-    expense_date_to = ''
-    params = "user="+user+"&expense_date_from="+expense_date_from+"&expense_date_to="+expense_date_to
-    print(params)
-    series_new=fetchExpenses(params)
-
-    return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis, pageType=pageType)
-
+@app.route("/chart")
+def chart():
+        user='nbalu@gmail.com'
+        expense_date_from=''
+        expense_date_to=''
+        params = "user="+user+"&expense_date_from="+expense_date_from+"&expense_date_to="+expense_date_to
+        print(params)
+        series_new=fetchExpenses(params)
+        print(type(series_new))
+        exp_dates = []
+        for d in series_new:
+            for k,v in d.items():
+                if k == 'expense_date':
+                    exp_dates.append(v)
+        print(exp_dates)
+        home_expenses = []
+        for d in series_new:
+            for k,v in d.items():
+                if k == 'home_expenses':
+                    home_expenses.append(int(v))
+        print(home_expenses)
+        medical_expenses = []
+        for d in series_new:
+            for k,v in d.items():
+                if k == 'medical_expenses':
+                    medical_expenses.append(int(v))
+        print(medical_expenses)
+        vehicle_expenses = []
+        for d in series_new:
+            for k,v in d.items():
+                if k == 'vehicle_expenses':
+                    vehicle_expenses.append(int(v))
+        print(vehicle_expenses)
+        legend1 = 'Home Expenses'
+        legend2 = 'Vehicle Expenses'
+        legend3 = 'Medical Expenses'
+        #labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        labels = exp_dates      
+        #values = [1000, 9000, 8000, 5000, 6000, 4000, 7000, 8000, 3000, 5500, 9000, 5000]       
+        
+        return render_template('chart.html', labels=labels, legend1=legend1, legend2=legend2, legend3=legend3, home_expenses=home_expenses, vehicle_expenses= vehicle_expenses, medical_expenses=medical_expenses )
+ 
 @app.route('/graph', methods=['GET','POST'])
 def graph(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
     user='nbalu@gmail.com'
